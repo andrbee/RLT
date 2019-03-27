8d5ea96350b9c1fd16fdccdf97468178721d90fd
andrbee <andrbee@gmail.com>
2019-03-28 00:31:56 +0300

    Refactoring scripts

--- src/Controller/AuctionController.php
+++ src/Controller/AuctionController.php
@@ -1,34 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: balashov_a
- * Date: 15.03.2019
- * Time: 11:26
- */
 
 namespace App\Controller;
 
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Auction;
 
-class AuctionController extends Controller {
-
+class AuctionController extends Controller
+{
     /**
-     * @Route("/auction/maps")
+     * @Route("/auction", name="auction")
      */
-    public function mapsAction(Request $request) {
-
-        if ($request->isXmlHttpRequest() || $request->query->get('urls')) {
-            $jsonData = array('aaa');
-
-            return new JsonResponse($jsonData);
-        } else {
-            die('DIE');
-        }
-
+    public function index()
+    {
+        die(1111);
 
     }
-
 }
